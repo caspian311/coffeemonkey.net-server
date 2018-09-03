56f487aadfd88f1d5c007b330da543af5b2cbf24
Matt Todd <matt.c.todd@gmail.com>
2018-09-03 09:52:07 -0500

    fixed camel casing issue

--- lib/controllers/chat_rooms.rb
+++ lib/controllers/chat_rooms.rb
@@ -7,5 +7,7 @@ end
 
 get '/chatRooms/:id' do
   halt_unless_valid_session
-  ChatRoom.find_by(id: 1).to_json(include: {chat_messages: {include: :user}})
+  chat_room = ChatRoom.find_by(id: 1).to_json(include: {chat_messages: {include: :user}})
+  parsed_data = JSON.parse chat_room
+  json parsed_data
 end
